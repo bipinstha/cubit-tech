4bea6ffd2d6bfe383d43a379d4fad09d562cdadd
tamubinod <tamu.bnod1@gmail.com>
2015-12-16 15:03:25 -0600

    UI Changes

--- alindus/src/main/resources/static/js/app.js
+++ alindus/src/main/resources/static/js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var myApp = angular.module('cubicApp', ['ui.bootstrap']);
+
+
+var myApp = angular.module('cubicApp', [ 'ngRoute', 'ui.bootstrap']);
+
+myApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : '/static/js/dashboard.html',
+		controller : 'dashboardCtrl'
+	}).when('/reports', {
+		templateUrl : '/static/js/reports.html',
+		controller : 'reportCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	})
+} ])
