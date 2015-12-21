e58bb7a337fa9acea2e255d1d1e80c549719818e
tamubinod <tamu.bnod1@gmail.com>
2015-12-21 12:31:18 -0600

    * Admin Page Template * Normal Routing Done

---
+++ alindus/src/main/resources/static/js/controller/userCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+myApp.controller('UserCtrl',['$scope','$routeParams','UserService',function ($scope, $routeParams, UserService) {
+	$scope.email = $routeParams.email;
+	$scope.user = {};
+	UserService.findUserByEmail($scope.email).then(function (response) {
+		$scope.user = response;
+	})
+}])
