1b422a4178226a79624855f65e0562506b133bdf
PrabinAdhikari <pabin.adhikari@gmail.com>
2015-12-22 10:30:41 -0600

    user detail page

--- alindus/src/main/resources/static/js/controller/userCtrl.js
+++ alindus/src/main/resources/static/js/controller/userCtrl.js
@@ -1,8 +1,9 @@
 'use strict';
 myApp.controller('UserCtrl',['$scope','$routeParams','UserService',function ($scope, $routeParams, UserService) {
 	$scope.email = $routeParams.email;
-	$scope.user = {};
+	//$scope.user = {};
 	UserService.findUserByEmail($scope.email).then(function (response) {
 		$scope.user = response;
+		console.log($scope.user);
 	})
 }])
