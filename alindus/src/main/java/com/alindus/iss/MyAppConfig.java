ca841f4cbd416aa8bf09ff97d0e16c341a628aca
bipinstha <aakee.stha@gmail.com>
2015-12-28 17:12:15 -0600

    * Cache configure for User Domain

--- alindus/src/main/java/com/alindus/iss/MyAppConfig.java
+++ alindus/src/main/java/com/alindus/iss/MyAppConfig.java
@@ -1,12 +1,18 @@
 package com.alindus.iss;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableCaching
 public class MyAppConfig extends WebMvcConfigurerAdapter {
 
 	@Bean // Magic entry
@@ -21,5 +27,17 @@ public class MyAppConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 	}
 
+	@Bean
+	public CacheManager cacheManager() {
+		return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+	}
+
+	@Bean
+	public EhCacheManagerFactoryBean ehCacheCacheManager() {
+		EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
+		cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
+		cmfb.setShared(true);
+		return cmfb;
+	}
 	
 }
--- alindus/src/main/java/com/alindus/iss/domain/Phone.java
+++ alindus/src/main/java/com/alindus/iss/domain/Phone.java
@@ -1,5 +1,7 @@
 package com.alindus.iss.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,8 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "phones")
-public class Phone {
+public class Phone implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
