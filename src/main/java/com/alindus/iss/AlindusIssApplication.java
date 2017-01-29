4d5ab85aeaa42271161d1142728f3775706ec5d6
Bipin Shrestha <bipinstha@Bipins-MacBook-Pro.local>
2017-01-29 03:39:08 -0500

    code changed for jboss appserver

--- src/main/java/com/alindus/iss/AlindusIssApplication.java
+++ src/main/java/com/alindus/iss/AlindusIssApplication.java
@@ -2,10 +2,17 @@ package com.alindus.iss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class AlindusIssApplication {
+public class AlindusIssApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AlindusIssApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AlindusIssApplication.class, args);
